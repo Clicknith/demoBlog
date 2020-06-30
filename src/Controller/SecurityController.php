22eb7bac940688821ae5d4c0f828ea5ea60e2f9a
Nithya NADARADJANE <nithhya.poovannnan@gmail.com>
2020-06-30 10:38:07 +0200

    morning+30062020

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegistrationType;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SecurityController extends AbstractController
 {
@@ -12,6 +14,12 @@ class SecurityController extends AbstractController
      */
     public function registration()
     {
+
+        $user = new User;
+
+        $form = $this->createForm(RegistrationType::class, $user);
+        
         return $this->render('security/registration.html.twig');
+            'form' => $form->createView()
     }
 }
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleType extends AbstractType
@@ -13,6 +15,10 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('title')
+            ->add('category', EntityType::class, [ // we are adding a new field here to select a category on adding or modifying an Article
+                'class' => Category::class, 
+                'choice_label' => 'title'
+            ])
             ->add('content')
             ->add('image')
         ;
